782c567d238ba99321f139595b2a3fbc6a5d1f37
Mattias Lindström <mattias.lindstrom@gmail.com>
2018-05-08 15:24:22 -0500

    Add second program

---
+++ ch2/pr2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define PI 3.141592653579
+
+
+// Computes the volume of a sphere with a 10 meter radius
+int main(void)
+{
+    printf("%.2f\n",4.0f / 3.0f * PI * 10 * 10 * 10);
+    return 0;
+}
