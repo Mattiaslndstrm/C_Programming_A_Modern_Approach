f7da119d058a6502b8608b70823a14ff9cc0b656
Mattias Lindström <mattias.lindstrom@gmail.com>
2018-05-08 15:51:36 -0500

    Spelling

--- ch2/pr5.c
+++ ch2/pr5.c
@@ -3,11 +3,12 @@
 // Calculates the result for 3x^5 + 2x^4 - 5x^3 - x^2 + 7x - 6 with the value
 // for x that the user provides.
 // Yes, unsafe use of scanf!
+
 int main(void)
 {
     double x;
     printf("3x^5 + 2x^4 - 5x^3 - x^2 + 7x - 6\n");
-    printf("Please enter x for the above polinomal: ");
+    printf("Please enter x for the above polynomial: ");
     scanf("%lf", &x);
     printf(
            "The result with x = %.0f is %.0f\n",
