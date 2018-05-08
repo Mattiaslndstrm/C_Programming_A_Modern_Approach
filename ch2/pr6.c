19a9db7cd4aab0c5ad817bccdd5a2abac4e4eddd
Mattias Lindström <mattias.lindstrom@gmail.com>
2018-05-08 15:51:52 -0500

    Add program #6

---
+++ ch2/pr6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Calculates the result for 3x^5 + 2x^4 - 5x^3 - x^2 + 7x - 6 with the value
+// for x that the user provides.
+// Yes, unsafe use of scanf!
+
+int main(void)
+{
+    double x;
+    printf("3x^5 + 2x^4 - 5x^3 - x^2 + 7x - 6\n");
+    printf("Please enter x for the above polinomal: ");
+    scanf("%lf", &x);
+    printf(
+           "The result with x = %.0f is %.0f\n",
+           x, ((((3 * x + 2) * x - 5) * x - 1) * x + 7) * x - 6
+           );
+    return 0;
+}
