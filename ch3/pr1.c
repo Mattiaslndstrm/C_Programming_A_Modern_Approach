c3da42c2037ef32a400dfc0e0bc07b27e8f0591b
Mattias Lindström <mattias.lindstrom@gmail.com>
2018-05-09 15:36:26 -0500

    Split lines

--- ch3/pr1.c
+++ ch3/pr1.c
@@ -6,7 +6,8 @@
 // the date in the format yyyy-mm-dd. Yes, unsafe use of scanf.
 
 int main(void)
-{   int day, month, year;
+{
+    int day, month, year;
     printf("Enter a date (dd/mm/yyyy):");
     scanf("%d/%d/%d/", &day, &month, &year);
     printf("You entered the date: %d-%.2d-%.2d\n", year, month, day);
