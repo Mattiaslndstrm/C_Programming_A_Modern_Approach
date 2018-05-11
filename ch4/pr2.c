46ec08e02f204a46f9f9c47af58b3fae2a9e7c9c
Mattias Lindström <mattias.lindstrom@gmail.com>
2018-05-11 13:12:44 -0500

    Add compact version of program 2

--- ch4/pr2.c
+++ ch4/pr2.c
@@ -3,12 +3,9 @@
 // Prints the reversal of a three-digit number. Unsafe use of scanf.
 int main(void)
 {
-    int input_number, hundred, ten, single;
+    int num;
     printf("Enter a three-digit number: ");
-    scanf("%d", &input_number);
-    hundred = input_number / 100;
-    ten = (input_number / 10) % 10;
-    single = (input_number) % 10;
-    printf("The reversal is: %d%d%d\n", single, ten, hundred);
+    scanf("%d", &num);
+    printf("The reversal is: %d%d%d\n", num % 10, (num / 10) % 10, num / 100);
     return 0;
 }
