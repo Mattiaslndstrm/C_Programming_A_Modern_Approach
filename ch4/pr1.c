746925167156ddeeeea37d944b5991d9c227c52b
Mattias Lindström <mattias.lindstrom@gmail.com>
2018-05-11 12:54:08 -0500

    Add program #1

---
+++ ch4/pr1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Return the reversal of a two-digit number. Unsafe use of scanf.
+int main(void)
+{
+    int input_number, ten, single;
+    printf("Enter a two-digit number: ");
+    scanf("%d", &input_number);
+    ten = input_number / 10;
+    single = input_number % 10;
+    printf("The reversal is: %d%d\n", single, ten);
+    return 0;
+}
