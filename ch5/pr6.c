b4c634a482827eb3a42590f18d49f69550ead51e
Mattias Lindström <mattias.lindstrom@gmail.com>
2018-05-25 15:55:15 -0500

    Fix indentation

--- ch5/pr6.c
+++ ch5/pr6.c
@@ -5,26 +5,26 @@
 // Unsafe use of scanf
 int main(void)
 {
-  int check_digit, d, i1, i2, i3, i4, i5, j1, j2, j3, j4, j5,
-      first_sum, second_sum, total;
+    int check_digit, d, i1, i2, i3, i4, i5, j1, j2, j3, j4, j5,
+    first_sum, second_sum, total;
 
-  printf("Enter the first (single) digit: ");
-  scanf("%1d", &d);
-  printf("Enter first group of five digits: ");
-  scanf("%1d%1d%1d%1d%1d", &i1, &i2, &i3, &i4, &i5);
-  printf("Enter second group of five digits: ");
-  scanf("%1d%1d%1d%1d%1d", &j1, &j2, &j3, &j4, &j5);
-  printf("Enter the last (single) digit: ");
-  scanf("%1d", &check_digit);
+    printf("Enter the first (single) digit: ");
+    scanf("%1d", &d);
+    printf("Enter first group of five digits: ");
+    scanf("%1d%1d%1d%1d%1d", &i1, &i2, &i3, &i4, &i5);
+    printf("Enter second group of five digits: ");
+    scanf("%1d%1d%1d%1d%1d", &j1, &j2, &j3, &j4, &j5);
+    printf("Enter the last (single) digit: ");
+    scanf("%1d", &check_digit);
 
-  first_sum = i1 + i3 + i5 + j2 + j4;
-  second_sum = i2 + i4 + j1 + j3 + j5;
-  total = 3 * first_sum + second_sum;
-  
-  if (check_digit == 9 - ((total -1) % 10))
-    printf("Valid\n");
-  else
-    printf("Not valid\n");
+    first_sum = i1 + i3 + i5 + j2 + j4;
+    second_sum = i2 + i4 + j1 + j3 + j5;
+    total = 3 * first_sum + second_sum;
 
-  return 0;
+    if (check_digit == 9 - ((total -1) % 10))
+        printf("Valid\n");
+    else
+        printf("Not valid\n");
+
+    return 0;
 }
