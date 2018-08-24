6b3b721df499b937260188b87a742e35b93ec849
Mattias Lindström <mattias.lindstrom@gmail.com>
2018-08-24 13:14:09 -0500

    Add solution to problem 5

---
+++ ch6/pr5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+// Returns the reverse of a number. Unsafe use of scanf.
+int main(void)
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    printf("Your number reversed is: ");
+    while (n > 0)
+    {
+        printf("%d", n % 10);
+        n /= 10;
+    }
+    printf("\n");
+}
