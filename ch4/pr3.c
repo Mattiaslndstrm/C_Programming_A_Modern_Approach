e92720036a6a2dca26db4d70d2d5e4ae8dea59e5
Mattias Lindström <mattias.lindstrom@gmail.com>
2018-10-03 12:22:56 -0500

    Add solution to problem 3

---
+++ ch4/pr3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+// Reverses a three digit number without arithmetic. Unsafe use of scanf.
+int main(void)
+{
+    int hundred, ten, singel;
+    printf("Enter a three-digit number: ");
+    scanf("%1d%1d%1d", &hundred, &ten, &singel);
+    printf("Your number reversed is %d%d%d\n", singel, ten, hundred);
+    return 0;
+}
+
+
