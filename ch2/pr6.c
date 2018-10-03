f0eeab450ce4ca442b0df66f28e74bd9a53c030b
Mattias Lindström <mattias.lindstrom@gmail.com>
2018-10-03 15:29:07 -0500

    Add changes

--- ch2/pr6.c
+++ ch2/pr6.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 // Calculates the result for 3x^5 + 2x^4 - 5x^3 - x^2 + 7x - 6 with the value
-// for x that the user provides.
+// for x that the user provides. Uses Horner's Rule for simplifying.
 // Yes, unsafe use of scanf!
 
 int main(void)
 {
     double x;
     printf("3x^5 + 2x^4 - 5x^3 - x^2 + 7x - 6\n");
-    printf("Please enter x for the above polinomal: ");
+    printf("Please enter x for the above polynomial: ");
     scanf("%lf", &x);
     printf(
            "The result with x = %.0f is %.0f\n",
