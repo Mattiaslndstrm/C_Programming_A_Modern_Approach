83ea11a400c7f01f9f1e1ff0acb723a493a3c4a6
Mattias Lindström <mattias.lindstrom@gmail.com>
2018-10-19 15:27:10 -0500

    Add solution to problem 6

--- ch5/pr6.c
+++ ch5/pr6.c
@@ -1,30 +1,20 @@
 #include <stdio.h>
 
 
-// Checks the validity of an UPC number
-// Unsafe use of scanf
+// Checks whether a UPC code is valid.
 int main(void)
 {
-    int check_digit, d, i1, i2, i3, i4, i5, j1, j2, j3, j4, j5,
-    first_sum, second_sum, total;
-
-    printf("Enter the first (single) digit: ");
-    scanf("%1d", &d);
-    printf("Enter first group of five digits: ");
-    scanf("%1d%1d%1d%1d%1d", &i1, &i2, &i3, &i4, &i5);
-    printf("Enter second group of five digits: ");
-    scanf("%1d%1d%1d%1d%1d", &j1, &j2, &j3, &j4, &j5);
-    printf("Enter the last (single) digit: ");
-    scanf("%1d", &check_digit);
-
-    first_sum = i1 + i3 + i5 + j2 + j4;
-    second_sum = i2 + i4 + j1 + j3 + j5;
+    int d, i1, i2, i3, i4, i5, j1, j2, j3, j4, j5, j6,
+        first_sum, second_sum, total, checkdigit;
+    printf("Enter a UPC code: ");
+    scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d",
+          &d, &i1, &i2, &i3, &i4, &i5, &j1, &j2, &j3, &j4, &j5, &j6);
+    first_sum = d + i2 + i4 + j1 + j3 + j5;
+    second_sum = i1 + i3 + i5 + j2 + j4;
     total = 3 * first_sum + second_sum;
-
-    if (check_digit == 9 - ((total -1) % 10))
-        printf("Valid\n");
+    checkdigit = 9 - ((total - 1) % 10);
+    if (j6 == checkdigit)
+        printf("The UPC code is valid\n");
     else
-        printf("Not valid\n");
-
-    return 0;
+        printf("The UPC code is invalid\n");
 }
