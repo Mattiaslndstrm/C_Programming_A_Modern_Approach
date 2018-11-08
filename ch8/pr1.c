f80ad9230b4eaa5e8f335daa277c3b4b0f9c6384
Mattias Lindström <mattias.lindstrom@gmail.com>
2018-11-08 17:28:43 -0600

    Add solution to ch8/pr1

---
+++ ch8/pr1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+// Prints the repeated digits in a number.
+// Unsafe use of scanf
+int main(void)
+{
+    bool digit_seen[10] = {false};
+    bool digit_double[10] = {false};
+    int digit;
+    long n;
+
+    printf("Enter a number: ");
+    scanf("%ld", &n);
+
+    while (n > 0)
+    {
+        digit = n % 10;
+        if (digit_seen[digit])
+            digit_double[digit] = true;
+        digit_seen[digit] = true;
+        n /= 10;
+    }
+    printf("Repeated digit(s): ");
+    for (int i = 0; i < 10; i++)
+        if (digit_double[i])
+            printf("%d ", i);
+    printf("\n");
+
+    return 0;
+}
